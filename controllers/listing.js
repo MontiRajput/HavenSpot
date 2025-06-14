8d836cb0cc2b431fba55f2808797ef16fbaf6982
Monti Rajput <montirajput@delta.in>
2025-06-14 17:52:04 +0530

    Fix: cloudinary version downgrade for Render

--- controllers/listing.js
+++ controllers/listing.js
@@ -44,8 +44,6 @@ module.exports.createListing = async (req, res, next) => {
   res.redirect("listings");
 };
 
-
-
 //edit
 module.exports.renderEditForm = async (req, res) => {
   let { id } = req.params;
