736dc7c8833dd8d3eb0365fc5566b0fc62a95d86
Monti Rajput <montirajput@delta.in>
2025-06-15 15:59:53 +0530

    Fix: cloudinary version downgrade for Render

--- app.js
+++ app.js
@@ -76,7 +76,9 @@ app.use((req, res, next) => {
   res.locals.currUser = req.user;
   next();
 });
-
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 app.use("/listings", listingRoute);
 app.use("/listings/:id/reviews", reviewRoute);
 app.use("/", userRoute);
