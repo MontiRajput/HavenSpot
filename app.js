c3893db7efd3a5359ba8f48048ac1b5a76b485f8
Monti Rajput <mrrajput7082@gmail.com>
2026-03-01 11:54:57 +0530

    update

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ passport.deserializeUser(User.deserializeUser()); //to delete user session data
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "https://havenspot.montirajput.space/auth/google/callback"
 },
 async (accessToken, refreshToken, profile, done) => {
   try {
